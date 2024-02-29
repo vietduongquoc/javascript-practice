c77783cb76ccb31cc4aa2d755ee714f7b78083de
VietDuong <duongquocviet@gmail.com>
2024-02-29 08:53:26 +0700

    Update style for page

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,5 +1,5 @@
 // Get the modal
-const modal = document.getElementById("myModal");
+const modal = document.getElementById("addProductModal");
 // Get the button that opens the modal
 const btn = document.getElementById("addBtn");
 // Get the <span> element that closes the modal
