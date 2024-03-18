ebcd5fee31ede0601dba872f03b38892430444ae
VietDuong <duongquocviet@gmail.com>
2024-03-18 17:44:37 +0700

    refactor-mvc-model

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,172 +1,34 @@
-import ProductView from './views/product.view';
-import { APIHandler } from './controllers/product.controller';
-import { API } from './constants/url-api';
-import validateForm from '../utils/validateProductForm';
-import generateErrorMessages from '../utils/dom';
-
-
-// import ProductController from "./controllers/product.controller";
-// import ProductModel from "./models/product.model";
-// import ProductView from "./views/product.view";
+import ProductController from "./controllers/product.controller";
+import ProductModel from "./models/product.model";
+import ProductView from "./views/product.view"
 
 // new ProductController(new ProductModel(), new ProductView());
 
 
-
-
-
 const homePage = document.querySelector('.homepage');
-// Get the modal
-
 // Get the button that opens the modal
 const btn = document.getElementById("addBtn");
-// Get the <span> element that closes the modal
-// When the user clicks the button, open the modal
 
 homePage.addEventListener('click', (e) => {
   const target = e.target;
   const modal = document.getElementById("addProductModal");
-
   if (target.id === 'addBtn' || target.id === "add-close") {
     modal.firstElementChild.reset();
     modal.classList.toggle('hidden');
   }
-
   else if (target.classList.contains('pagination-link')) {
     const url = target.getAttribute('href');
     e.preventDefault();
     window.history.pushState(null, '', url);
-
     const page = target.textContent;
     ProductView.currentPage = parseInt(page);
 
-    APIHandler.get()
+    ProductModel.get()
       .then(data => {
         ProductView.renderProducts(data);
       })
       .catch(error => console.error('Failed to load products:', error));
   }
-
 });
-
-
-
-document.addEventListener('DOMContentLoaded', async function () {
-  const dataLength = await APIHandler.getDataLength();
-  ProductView.totalPages = parseInt(dataLength / 8) + 1;
-
-  APIHandler.get()
-    .then(data => {
-      ProductView.renderProducts(data);
-    })
-    .catch(error => console.error('Failed to load products:', error));
-});
-
-addProductModal.addEventListener('submit', async function (event) {
-  event.preventDefault();
-
-  // Get the values from the form inputs
-  const nameValue = document.getElementById('productName').value;
-  const TypeValue = document.getElementById('productType').value;
-  const QuantityValue = document.getElementById('productQuantity').value;
-  const priceValue = document.getElementById('productPrice').value;
-  const brandValue = document.getElementById('productBrand').value;
-
-  // Create a product object with the form input values
-  const productInputs = {
-    'Name': nameValue,
-    'Price': priceValue,
-    'Brand': brandValue,
-    'Type': TypeValue,
-    'Quantity': QuantityValue,
-  }
-
-  const { formError } = validateForm(productInputs);
-
-  // Generate new error messages based on the validation results
-  generateErrorMessages(formError);
-
-  // If there are any validation errors, stop the function
-  const isPassed = Object.values(formError).every(value => value === '');
-  if (!isPassed) {
-    return;
-  }
-
-  const product = {
-    name: nameValue,
-    price: priceValue,
-    brand: brandValue,
-    type: TypeValue,
-    quantity: QuantityValue
-  }
-  let targetPage = 1;
-  // Send a new data product to the API and process the results
-  try {
-    const dataLength = await APIHandler.getDataLength();
-    targetPage = parseInt((dataLength / 8)) + 1;
-
-    await APIHandler.post('products', product);
-
-    ProductView.totalPages = parseInt((dataLength / 8)) + 1;
-
-  } catch (error) {
-    console.error('Error adding product:', error);
-  }
-
-  // Get the list of new products after adding
-  try {
-    const data = await APIHandler.get({ page: targetPage });
-    addProductModal.classList.toggle('hidden');
-    ProductView.renderProducts(data);
-  } catch (error) {
-    console.error('Failed to load products:', error);
-  };
-
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-  const editProductModal = document.getElementById('editProductModal');
-  editProductModal.addEventListener('submit', async function (e) {
-    e.preventDefault();
-
-    // Assuming you have a way to set and get the currently editing product's ID.
-    const productId = e.target.getAttribute('data-product-id');
-
-    // console.log(productId)
-    const editProductName = document.getElementById('edit-productName').value;
-    const editProductQuantity = document.getElementById('edit-productQuantity').value;
-    const editProductType = document.getElementById('edit-productType').value;
-    const editProductPrice = document.getElementById('edit-productPrice').value;
-    const editProductBrand = document.getElementById('edit-productBrand').value;
-
-    const editedProductData = {
-      name: editProductName,
-      quantity: editProductQuantity,
-      type: editProductType,
-      price: editProductPrice,
-      brand: editProductBrand
-    };
-    console.log('editedProductData: ', editedProductData)
-    // Send the edited product data to the API and process the results
-    try {
-      const updatedProduct = await APIHandler.editProduct(productId, editedProductData);
-
-      // Assuming renderEditProduct is similar to renderNewProduct but for updating the UI with the edited product details.
-      // If renderNewProduct can handle both new and updated products, you can call it directly instead.
-      ProductView.renderEditProduct(updatedProduct);
-    } catch (error) {
-      console.error('Error editing product:', error);
-    }
-    // Optionally, fetch and refresh the list of products.
-    try {
-      const data = await APIHandler.get('products');
-      ProductView.renderProducts(data); // Assuming this method exists to render all products
-    } catch (error) {
-      console.error('Failed to load products:', error);
-    };
-    location.reload(); // Or close the modal and update the UI as needed without reloading.
-  });
-});
-
 
 
---
+++ src/javascripts/models/product.model.js
@@ -0,0 +1,102 @@
+import { API } from '../constants/url-api';
+
+export default class ProductModel {
+  static async getDataLength() {
+    const url = new URL(`https://65dbf3583ea883a15292483f.mockapi.io/api/products`);
+    try {
+      // Use the generated URL with query parameters for the fetch call
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const data = await response.json();
+      const dataLength = data.length;
+      return dataLength;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  static async get() {
+    const queryString = window.location.search;
+    const url = new URL(`https://65dbf3583ea883a15292483f.mockapi.io/api/products`);
+    const urlParams = new URLSearchParams(queryString);
+    const page = urlParams.get('page');
+    url.searchParams.append('page', page || 1);
+    url.searchParams.append('limit', '8');
+    try {
+      // Use the generated URL with query parameters for the fetch call
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  static async post(endpoint, product) {
+    try {
+      const res = await fetch(`${API.BASE_URL}/${endpoint}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(product)
+      });
+      if (!res.ok) {
+        throw new Error(`Failed to post data to ${API.BASE_URL}/${endpoint}`);
+      }
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  static async editProduct(productId, editedProductData) {
+    const response = await fetch(`${API.BASE_URL}/products/${productId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(editedProductData),
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return await response.json();
+  }
+  static async deleteProduct(productId) {
+    // console.log('deleteProduct: ', productId)
+    try {
+      const { BASE_URL, PRODUCTS_ENDPOINT } = API;
+      const url = `${BASE_URL}/${PRODUCTS_ENDPOINT}/${productId}`;
+      const res = await fetch(url, {
+        method: 'DELETE',
+      });
+      if (!res.ok) {
+        throw new Error(`Failed to delete product with ID: ${productId}`);
+      }
+      console.log('Delete successfully!');
+      return { isSuccess: true };
+    } catch (error) {
+      console.error(error);
+      console.error('Error deleting product:', error.message);
+      return { isSuccess: false };
+    }
+  }
+}
+
